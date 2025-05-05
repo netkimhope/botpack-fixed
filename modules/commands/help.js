f50740303c2e3c29d61c7df1a3edf06531958942
netkimhope <168548012+netkimhope@users.noreply.github.com>
2025-05-05 12:23:01 +0800

    Update help.js

--- modules/commands/help.js
+++ modules/commands/help.js
@@ -1,100 +1,209 @@
 module.exports.config = {
-  name: 'help',
-  version: '1.0.0',
-  role: 0,
-  aliases: ['info', 'menu'],
-  usage: '[command]',
-  info: "Beginner's guide",
-  credits: 'Developer',
+  name: "help",
+  version: "1.0.2",
+  hasPermission: 0,
+  credits: "developer",
+  description: "Beginner's Guide",
+  usePrefix: true,
+  commandCategory: "guide",
+  usages: "[Shows Commands]",
+  cooldowns: 5,
+  envConfig: {
+		autoUnsend: true,
+		delayUnsend: 60
+	}
 };
 
-module.exports.run = async function ({ api, event, Utils, prefix, args, chat, fonts }) {
-  const input = args.join(' ').trim().toLowerCase();
-  const tin = txt => fonts.thin(txt);
-  const allCommands = [...Utils.handleEvent.values(), ...Utils.commands.values()];
-  const perPage = 11;
+module.exports.languages = {
+  en: {
+    moduleInfo:
+      "「 %1 」\n%2\n\n❯ Usage: %3\n❯ Category: %4\n❯ Waiting time: %5 seconds(s)\n❯ Permission: %6\n\n» Module code by %7 ",
+    helpList:
+      `◖There are %1 commands and %2 categories on this bot.`,
+    guideList:
+      `◖Use: "%1${this.config.name} ‹command›" to know how to use that command!\n◖Type: "%1${this.config.name} ‹page_number›" to show that page contents!`,
+    user: "User",
+    adminGroup: "Admin group",
+    adminBot: "Admin bot",
+  },
+};
 
-  try {
-    const totalCommands = allCommands.length;
 
-    if (!input) {
-      let helpMessage = `📋 | CMDS List: 〔${prefix || 'no prefix'}〕\n`;
-      helpMessage += `Total Commands: ${totalCommands}🏷️\n\n`;
+module.exports.handleEvent = function ({ api, event, getText }) {
+  const { commands } = global.client;
+  const { threadID, messageID, body } = event;  
 
-      const firstPageCommands = allCommands.slice(0, perPage);
-      firstPageCommands.forEach((command, index) => {
-        const { name, info, usage } = command;
-        helpMessage += `\t${index + 1}. ${name} ${usage ? `${usage}` : ''}\n`;
-      });
+  if (!body || typeof body == "undefined" || body.indexOf("help") != 0)
+    return;
+  const splitBody = body.slice(body.indexOf("help")).trim().split(/\s+/);
+  if (splitBody.length == 1 || !commands.has(splitBody[1].toLowerCase())) return;
+  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+  const command = commands.get(splitBody[1].toLowerCase());
+  const prefix = threadSetting.hasOwnProperty("PREFIX")
+    ? threadSetting.PREFIX
+    : global.config.PREFIX;
+  return api.sendMessage(
+    getText(
+      "moduleInfo",
+      command.config.name,
+      command.config.description,
+      `${prefix}${command.config.name} ${
+        command.config.usages ? command.config.usages : ""
+      }`,
+      command.config.commandCategory,
+      command.config.cooldowns,
+      command.config.hasPermission === 0
+        ? getText("user")
+        : command.config.hasPermission === 1
+        ? getText("adminGroup")
+        : getText("adminBot"),
+      command.config.credits
+    ),
+    threadID,
+    messageID
+  );
+};
 
-      helpMessage += `\nFor all cmds, type '${prefix || ''}help all'\n`;
-     // helpMessage += `To see another page, use '${prefix || ''}help [page-number]'\n`;
-      chat.reply(tin(helpMessage));
-    } else if (input === 'all') {
-      let helpMessage = `📋 | CMDS List: 〔${prefix || 'no prefix'}〕\n`;
-      helpMessage += `Total Commands: ${totalCommands}🏷️\n\n`;
+module.exports.run = async function ({ api, event, args, getText }) {
+  const { commands } = global.client;
+  const { threadID, messageID } = event;
+  const command = commands.get((args[0] || "").toLowerCase());
+  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+  const { autoUnsend, delayUnsend } = global.configModule[this.config.name];
+  const prefix = threadSetting.hasOwnProperty("PREFIX")
+    ? threadSetting.PREFIX
+    : global.config.PREFIX;
 
-      allCommands.forEach((command, index) => {
-        const { name, info, usage } = command;
-        helpMessage += `\t${index + 1}. ${name} ${usage ? `${usage}` : ''}\n`;
-      });
+  if (!command) {
+    const commandList = Array.from(commands.values());
+    const categories = new Set(commandList.map((cmd) => cmd.config.commandCategory.toLowerCase()));
+    const categoryCount = categories.size;
 
-      chat.reply(tin(helpMessage));
-    } else if (!isNaN(input)) {
-      const page = parseInt(input);
-      const totalPages = Math.ceil(totalCommands / perPage);
+    const categoryNames = Array.from(categories);
+    const itemsPerPage = 10;
+    const totalPages = Math.ceil(categoryNames.length / itemsPerPage);
 
-      if (page < 1 || page > totalPages) {
-        chat.reply(`Invalid page number. Please specify a page between 1 and ${totalPages}.`);
-        return;
-      }
-
-      const startIndex = (page - 1) * perPage;
-      const endIndex = Math.min(startIndex + perPage, totalCommands);
-      const commandsOnPage = allCommands.slice(startIndex, endIndex);
-
-      let helpMessage = `📋 | CMDS List: Page ${page}/${totalPages}\n`;
-      helpMessage += `Total Commands: ${totalCommands}🏷️\n\n`;
-
-      commandsOnPage.forEach((command, index) => {
-        const { name, info, usage } = command;
-        helpMessage += `\t${startIndex + index + 1}. ${name} ${usage ? `${usage}` : ''}\n`;
-      });
-
-      //helpMessage += `\nTo see another page, use '${prefix || ''}help [page-number]'\n`;
-      chat.reply(tin(helpMessage));
-    } else {
-      const selectedCommand = allCommands.find(command => {
-        const aliases = command?.aliases || [];
-        return command.name.toLowerCase() === input || aliases.includes(input);
-      });
-
-      if (selectedCommand) {
-        const { name, version, role, aliases = [], info, usage, credits, cd } = selectedCommand;
-        const roleMessage = role !== undefined ? `Role: ${role}` : '';
-        const aliasesMessage = aliases.length ? `Aliases: ${aliases.join(', ')}\n` : '';
-        const descriptionMessage = info ? `Info: ${info}\n` : '';
-        const usageMessage = usage ? `Usage: ${usage}\n` : '';
-        const creditsMessage = credits ? `Author: ${credits}\n` : '';
-        const versionMessage = version ? `Version: ${version}\n` : '';
-        const cooldownMessage = cd ? `Cooldown: ${cd} second(s)\n` : '';
-        const message = `COMMAND\n\nName: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
-        chat.reply(tin(message));
+    let currentPage = 1;
+    if (args[0]) {
+      const parsedPage = parseInt(args[0]);
+      if (
+        !isNaN(parsedPage) &&
+        parsedPage >= 1 &&
+        parsedPage <= totalPages
+      ) {
+        currentPage = parsedPage;
       } else {
-        chat.reply(tin(`Command '${input}' not found. Use '${prefix || '' }help' to see available commands.`));
+        return api.sendMessage(
+          `◖Oops! You went too far! Please choose a page between 1 and ${totalPages}◗`,
+          threadID,
+          messageID
+        );
       }
     }
-  } catch (error) {
-    console.error(error);
-  }
+    const startIdx = (currentPage - 1) * itemsPerPage;
+    const endIdx = startIdx + itemsPerPage;
+    const visibleCategories = categoryNames.slice(startIdx, endIdx);
+
+    let msg = "";
+    for (let i = 0; i < visibleCategories.length; i++) {
+      const category = visibleCategories[i];
+      const categoryCommands = commandList.filter(
+        (cmd) =>
+          cmd.config.commandCategory.toLowerCase() === category
+      );
+      const commandNames = categoryCommands.map((cmd) => cmd.config.name);
+      const numberFont = [
+        "❶",
+        "❷",
+        "❸",
+        "❹",
+        "❺",
+        "❻",
+        "❼",
+        "❽",
+        "❾",
+        "❿",
+      ];
+      msg += `╭[ ${numberFont[i]} ]─❍ ${
+        category.charAt(0).toUpperCase() + category.slice(1)
+      }\n╰─◗ ${commandNames.join(", ")}\n\n`;
+    }
+
+    const numberFontPage = [
+      "❶",
+      "❷",
+      "❸",
+      "❹",
+      "❺",
+      "❻",
+      "❼",
+      "❽",
+      "❾",
+      "❿",
+      "⓫",
+      "⓬",
+      "⓭",
+      "⓮",
+      "⓯",
+      "⓰",
+      "⓱",
+      "⓲",
+      "⓳",
+      "⓴",
+    ];
+    msg += `╭ ──────── ╮
+│ Page ${numberFontPage[currentPage - 1]} of ${
+      numberFontPage[totalPages - 1]
+    } │\n╰ ──────── ╯\n`;
+    msg += getText("helpList", commands.size, categoryCount, prefix);
+
+    const axios = require("axios");
+    const fs = require("fs-extra");
+    const imgP = [];
+    const img = [
+      "https://i.ibb.co/VcNdjtmr/help.png"
+    ];
+    const path = __dirname + "/cache/menu.png";
+    const rdimg = img[Math.floor(Math.random() * img.length)];
+
+    const { data } = await axios.get(rdimg, {
+      responseType: "arraybuffer",
+    });
+
+    fs.writeFileSync(path, Buffer.from(data, "utf-8"));
+    imgP.push(fs.createReadStream(path));
+    const config = require("./../../config.json")
+    const msgg = {
+  body: `╭──────────────╮\n│ 𝖢𝗈𝗆𝗆𝖺𝗇𝖽/𝖢𝖺𝗍𝖾𝗀𝗈𝗋𝗒 │\n╰──────────────╯\n‣ Bot Owner: ${config.DESIGN.Admin}\n\n` + msg + `\n◖Total pages available: ${totalPages}.\n` + `\n╭ ────── ╮\n│    GUIDE    │\n╰ ────── ╯\n` + getText("guideList", config.PREFIX),
+  attachment: imgP,
 };
 
-module.exports.handleEvent = async function ({ event, prefix, chat, fonts }) {
-  const { body } = event;
-  const tin = txt => fonts.thin(txt);
-  const message = prefix ? 'This is my prefix: ' + prefix : "Sorry, I don't have a prefix";
+    const sentMessage = await api.sendMessage(msgg, threadID, messageID);
 
-  if (body?.toLowerCase().startsWith('prefix')) {
-    chat.reply(tin(message));
+    if (autoUnsend) {
+      setTimeout(async () => {
+        await api.unsendMessage(sentMessage.messageID);
+      }, delayUnsend * 1000);
+    }
+  } else {
+    return api.sendMessage(
+      getText(
+        "moduleInfo",
+        command.config.name,
+        command.config.description,
+        `${prefix}${command.config.name} ${
+          command.config.usages ? command.config.usages : ""
+        }`,
+        command.config.commandCategory,
+        command.config.cooldowns,
+        command.config.hasPermission === 0
+          ? getText("user")
+          : command.config.hasPermission === 1
+          ? getText("adminGroup")
+          : getText("adminBot"),
+        command.config.credits
+      ),
+      threadID, messageID
+    );
   }
-}
+};
